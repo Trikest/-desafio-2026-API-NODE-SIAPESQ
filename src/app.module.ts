d4eea62597e063698af165e89e856fc23009999a
Trikest <salmo.00000850311@unicap.br>
2026-05-04 18:00:35 -0300

    feat: adiciona estrutura básica e schema do Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { SpeciesModule } from './species/species.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, SpeciesModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
